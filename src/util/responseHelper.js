3f46e9674635b46ed76a7224c9993f88f600b2f5
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-08-08 15:59:12 +0100

    Info: ResponseHelper File

---
+++ src/util/responseHelper.js
@@ -0,0 +1,26 @@
+// Response Helper
+export default class Response_Helper {
+
+    /**
+    * @description - this is used to return an error response
+    * @param {object} res - the object response
+    * @param {number} statuscode - the statuscode 
+    * @param {string} message - the message to be sent
+    * @param {object} data - the object data
+    */
+    static successResponse(res, statusCode, message, data) {
+        return res.status(statusCode).json({ status: 'success', message, data });
+    }
+
+
+    /**
+     * @description - this is used to return an error response
+     * @param {object} res - the object response
+     * @param {number} statuscode - the statuscode 
+     * @param {string} message - the message to be sent
+     */
+    static errorResponse(res, statusCode, message, data) {
+        return res.status(statusCode).json({ status: 'success', message });
+    }
+}
+
