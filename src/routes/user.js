168446ede62ac384595dbb913b6a9c9aabdbd2aa
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-08-24 14:32:25 +0100

    Info: Updated User Routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,39 +4,54 @@ const router = new Router()
 const User_Controller = require('../controller/user.js')
 const Authorization = require('../middleware/authorization.js')
 const { upload } = require('../util/multer.js')
+const SchemaValidationHelper = require('../validation/schemaValidationHelper.js')
+const {
+    userInputs,
+    validateInput,
+    validateUpdateInput,
+    validateYourInput,
+    validateResetInput,
+} = require('../validation/schema/user.js')
 
-// get
+// Get
+router.get('/:id/get/a/user', Authorization, User_Controller.getAUserById)
 router.get(
     '/search/for/users',
     Authorization,
     User_Controller.search_for_users_with_username_or_role
 )
+router.get('/getProfile', Authorization, User_Controller.getProfile)
+router.get('/all/admins', Authorization, User_Controller.getAllAdmins)
+router.get('/all/agents', Authorization, User_Controller.getAllAgents)
 
-// post
-router.post('/signUp', User_Controller.registerUser)
-router.post('/signIn', User_Controller.signIn)
+// Post
 router.post(
-    '/:id/upload/profile/picture',
+    '/signUp',
+    SchemaValidationHelper.validate(userInputs),
+    User_Controller.registerUser
+)
+router.post('/signIn', User_Controller.signIn)
+router.post('/logOut', User_Controller.logOut)
+router.post(
+    '/upload/profile/picture',
     Authorization,
     upload.single('image'),
     User_Controller.uploadYourProfilePicture
 )
-router.post(
-    '/:id/add/to/favorites',
-    Authorization,
-    User_Controller.addToFavorites
-)
-router.post(
-    '/:id/remove/from/favorites',
-    Authorization,
-    User_Controller.removeFromFavorites
-)
+router.post('/forgotten/password', User_Controller.forgottenPassword)
 
-// put
+// Put
 router.put(
     '/update/your/profile',
     Authorization,
+    SchemaValidationHelper.validate(validateYourInput),
     User_Controller.updateYourProfile
+)
+router.put(
+    '/:id/update',
+    Authorization,
+    SchemaValidationHelper.validate(validateUpdateInput),
+    User_Controller.updateUserById
 )
 router.put(
     '/update/your/profile/picture',
@@ -44,14 +59,24 @@ router.put(
     upload.single('image'),
     User_Controller.updateYourProfilePicture
 )
+router.put(
+    '/change/password',
+    Authorization,
+    SchemaValidationHelper.validate(validateInput),
+    User_Controller.changePassword
+)
+router.put(
+    '/reset/:id/password',
+    SchemaValidationHelper.validate(validateResetInput),
+    User_Controller.resetPassword
+)
 
-// delete
+// Delete
 router.delete(
     '/delete/your/account',
     Authorization,
     User_Controller.deleteYourAccount
 )
-
-//TODO remember to create : forgot password, reset password , get a user by id, update a user by id, delete a user by id
+router.delete('/:id/delete', Authorization, User_Controller.deleteAUserById)
 
 module.exports = router
