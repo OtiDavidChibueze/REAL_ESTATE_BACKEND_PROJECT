718d0826b6efedcd82ef6ecc10c3cd59e93cddff
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-08-24 14:31:31 +0100

    Info: Updated User Models

--- src/model/user.js
+++ src/model/user.js
@@ -25,6 +25,10 @@ const userSchema = new mongoose.Schema(
         password: {
             type: String,
         },
+        phonenumber: {
+            type: String,
+            unique: true,
+        },
         role: {
             type: String,
             enum: ['admin', 'agent', 'buyer', 'owner'],
